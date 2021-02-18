a917893b30771b6d2e8fd29b35295162086b6caa
lamine540 <camaralamine540@gmai.com>
2021-02-18 03:03:38 +0000

    Mise a jour

--- src/app/group-competence/list-grp-competence/list-grp-competence.component.ts
+++ src/app/group-competence/list-grp-competence/list-grp-competence.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-list-grp-competence',
@@ -7,20 +6,10 @@ import {Router} from '@angular/router';
   styleUrls: ['./list-grp-competence.component.css']
 })
 export class ListGrpCompetenceComponent implements OnInit {
-  groupedeCompetence: GroupeDeCompetences[] = [];
-  constructor(private groupCompetenceService : GroupetenceService,
-  private router: Router
-  ) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.getAllgrpecompetences();
-  }
-  getAllgrpecompetences(): any{
-   return this.grpecompetencesService.getGroupCompetence().subscribe((data: any) => {
-     this.grpecompetences = data;
-     console.log(this.grpecompetences)
-     }
-   )
   }
 
 }
